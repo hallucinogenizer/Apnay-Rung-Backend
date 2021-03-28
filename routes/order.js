3845451f20498ce01043463f5f789630ea45c12c
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-03-28 22:04:16 +0500

    order/all for all users

--- routes/order.js
+++ routes/order.js
@@ -8,6 +8,7 @@ router.post('/new', authenticateJWT, (req, res) => {
     /* 
     {
         items:{[item_id,quantity,price],[1,2,500],[]},
+        seller_ids:[],
         totalamount:0,
         delivery_status:'processing',
         name:'Rohan',
@@ -18,8 +19,8 @@ router.post('/new', authenticateJWT, (req, res) => {
     }
     */
     if (req.userObject.typeOfUser == "seller") {
-        const query = "INSERT INTO orders (timestamp,customer_id,delivery_status,review,totalamount,cancelled,items,name,email,phone,b_address,s_address) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)"
-        const values = ['NOW()', req.userObject.id, req.body.delivery_status, "", req.body.totalamount, false, req.body.items, req.body.name, req.body.email, req.body.phone, req.body.b_address, req.body.s_address]
+        const query = "INSERT INTO orders (timestamp,customer_id,delivery_status,review,totalamount,cancelled,items,name,email,phone,b_address,s_address,seller_ids) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)"
+        const values = ['NOW()', req.userObject.id, req.body.delivery_status, "", req.body.totalamount, false, req.body.items, req.body.name, req.body.email, req.body.phone, req.body.b_address, req.body.s_address, req.body.seller_ids]
 
         client.query(query, values)
             .then(response => {
@@ -30,6 +31,45 @@ router.post('/new', authenticateJWT, (req, res) => {
     } else {
         console.log(req.userObject.typeOfUser)
         res.sendStatus(401)
+    }
+})
+
+router.get('/all', authenticateJWT, (req, res) => {
+    if (req.userObject.typeOfUser == "admin") {
+        const query = { text: "SELECT * FROM orders WHERE true", values: [] }
+        client.query(query)
+            .then(result => {
+                res.status(200).json(result.rows)
+            })
+            .catch(err => {
+                res.sendStatus(500)
+                console.log(err)
+            })
+
+    } else if (req.userObject.typeOfUser == "customer") {
+        const query = {
+            text: "SELECT * FROM orders WHERE customer_id=$1",
+            values: [req.userObject.id]
+        }
+        client.query(query)
+            .then(result => {
+                res.status(200).json(result.rows).end()
+            })
+            .catch(err => {
+                res.sendStatus(500)
+            })
+    } else if (req.userObject.typeOfUser == "seller") {
+        const query = {
+            text: `SELECT * FROM orders WHERE ${req.userObject.id}=ANY(orders.seller_ids)`,
+            values: []
+        }
+        client.query(query)
+            .then(result => {
+                res.status(200).json(result.rows).end()
+            })
+            .catch(err => {
+                res.sendStatus(500)
+            })
     }
 })
 
