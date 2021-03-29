d470df3625a8dcd2eccf10c391d1850cef0d13f0
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-03-30 03:09:51 +0500

    changed connection string

--- utilities/clientConnect.js
+++ utilities/clientConnect.js
@@ -1,10 +1,8 @@
-require('dotenv').config()
-
 const { Client } = require('pg')
 
 
 const client = new Client({
-    process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
