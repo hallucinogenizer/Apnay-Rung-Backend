ecd0911a97fa239c217eb03c7364e5e474851471
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-02 13:46:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ const port = 3000
 
 //middleware
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use('/customer', customer)
 app.use('/admin', admin)
 app.use('/seller', seller)
