cfeecacd84594ddc61023566e020f166218e8aec
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-10 13:44:58 +0500

    added single verify for all types of users

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { query } = require('express')
 const path = require('path');
 require('dotenv').config()
 const cors = require('cors')
-
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 //routes
 const customer = require('./routes/customer')
@@ -20,7 +21,9 @@ const ejs = require('ejs')
 const authenticateJWT = require('./utilities/authenticateJWT')
 
 //expressJS
-const express = require('express')
+const express = require('express');
+const client = require('./utilities/clientConnect');
+const e = require('express');
 const app = express()
 const port = 3000
 
@@ -49,6 +52,107 @@ app.get('/', (req, res) => {
 
 app.get('/fileupload', (req, res) => {
     res.render(path.join(__dirname + '/views/fileupload.html'))
+})
+
+app.post('/verify', async(req, res) => {
+    const query = `SELECT customer_id,name,password FROM customers WHERE email='${req.body.email}' AND blocked=false;`
+    try {
+        const result = await client.query(query)
+        let userObject = {
+            id: -1,
+            name: '',
+            typeOfUser: 'customer'
+        }
+
+        if (result.rowCount > 0) {
+            let promises = []
+            for (let row of result.rows) {
+                promises.push(bcrypt.compare(req.body.password, row.password))
+                userObject.id = row.customer_id
+                userObject.name = row.name
+            }
+            Promise.all(promises)
+                .then(async(resolve) => {
+                    if (resolve.includes(true)) {
+                        const accessToken = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET)
+                        res.status(200).json({ verified: true, typeOfUser: 'customer', accessToken: accessToken }).end()
+                    } else {
+                        res.status(200).json({ verified: false }).end()
+                    }
+                })
+                .catch(err => {
+                    res.sendStatus(500)
+                    console.log(err)
+                })
+        } else {
+            query = "SELECT seller_id, name, password FROM sellers WHERE email=$1"
+            values = [req.body.email]
+            result = await client.query(query, values)
+            if (result.rowCount == 0) {
+
+                query = "SELECT admin_id, name, password FROM admins WHERE email=$1"
+                values = [req.body.email]
+                result = await client.query(query, values)
+                if (result.rowCount == 0) {
+                    res.status(200).json({ verified: false }).end()
+                } else if (result.rowCount == 1) {
+                    userObject = {
+                        id: -1,
+                        name: '',
+                        typeOfUser: 'admin'
+                    }
+                    let promises = []
+                    for (let row of result.rows) {
+                        promises.push(bcrypt.compare(req.body.password, row.password))
+                        userObject.id = row.admin_id
+                        userObject.name = row.name
+                    }
+                    Promise.all(promises).then(resolve => {
+                        if (resolve.includes(true)) {
+                            const accessToken = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET)
+                            res.status(200).json({ verified: true, typeOfUser: 'admin', accessToken: accessToken }).end()
+                        } else {
+                            res.status(200).json({ verified: false }).end()
+                        }
+                    })
+                }
+
+
+
+
+
+
+
+
+
+
+            } else if (result.rowCount == 1) {
+                userObject = {
+                    id: -1,
+                    name: '',
+                    typeOfUser: 'seller'
+                }
+                let promises = []
+                for (let row of result.rows) {
+                    promises.push(bcrypt.compare(req.body.password, row.password))
+                    userObject.id = row.seller_id
+                    userObject.name = row.name
+                }
+                Promise.all(promises).then(resolve => {
+                    if (resolve.includes(true)) {
+                        const accessToken = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET)
+                        res.status(200).json({ verified: true, typeOfUser: 'seller', accessToken: accessToken }).end()
+                    } else {
+                        res.status(200).json({ verified: false }).end()
+                    }
+                })
+            }
+
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 app.listen(process.env.PORT || port, () => {
