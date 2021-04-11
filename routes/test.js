52dc257f69e9ef2f4dcfdcd6046a1e8452389fe7
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-11 16:08:54 +0500

    update seller profile picture

--- routes/test.js
+++ routes/test.js
@@ -20,7 +20,7 @@ const client = require('../utilities/clientConnect')
 
 router.get('/read', (req, res) => {
 
-    const query = `UPDATE inventory SET image=$1 WHERE item_id=$2`
+    const query = `UPDATE sellers SET profile_picture=$1 WHERE seller_id=$2`
     const values = [req.body.image, req.body.seller_id]
 
     client.query(query, values).then(response => {
