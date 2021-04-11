d05fff40519ab9ffaae787876a92b85f552288ee
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-11 18:08:56 +0500

    remove slashes from profile_picture attempt 2

--- routes/test.js
+++ routes/test.js
@@ -21,9 +21,7 @@ const client = require('../utilities/clientConnect')
 router.get('/read', (req, res) => {
 
     const query = `UPDATE sellers SET profile_picture=$1 WHERE seller_id=$2`
-        // console.log()
-    const values = [JSON.stringify(req.body.image), req.body.seller_id]
-
+    const values = [req.body.image, req.body.seller_id]
     client.query(query, values).then(response => {
         res.end()
         console.log(response)
