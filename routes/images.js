afda67d1c5d87f09bffc5c8c9c6ca38f3cb593c3
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-22 02:20:16 +0500

    added image/seller/:seller_id

--- routes/images.js
+++ routes/images.js
@@ -21,4 +21,23 @@ router.get('/item/:item_id', (req, res) => {
         })
 })
 
+router.get('/seller/:seller_id', (req, res) => {
+    const query = "SELECT profile_picture AS data FROM sellers WHERE seller_id=$1"
+    const values = [req.params.seller_id]
+    client.query(query, values)
+        .then(result => {
+            if (result.rowCount > 0) {
+                res.contentType('jpeg')
+                res.status(200)
+                res.end(result.rows[0].data)
+            } else {
+                res.sendStatus(204)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.sendStatus(500)
+        })
+})
+
 module.exports = router
