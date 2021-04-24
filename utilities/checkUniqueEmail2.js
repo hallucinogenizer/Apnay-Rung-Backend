879403de5b00815ab9a1a1c7c0d922850e2fb0ee
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-25 00:35:28 +0500

    debugging

--- utilities/checkUniqueEmail2.js
+++ utilities/checkUniqueEmail2.js
@@ -12,32 +12,26 @@ const checkUniqueEmail2 = async(email, usertype, user_id) => {
         if (result.rowCount > 0) {
             console.log("1")
             return false
+        }
+    } else if (usertype == 'seller') {
+        query = "SELECT seller_id FROM sellers WHERE email=$1 AND seller_id!=$2"
+        values = [email, user_id]
+        result = await client.query(query, values)
+        if (result.rowCount > 0) {
+            console.log("3")
+            console.log(result.rows[0])
+            return false
+        }
+    } else if (usertype == 'admin') {
+        query = "SELECT admin_id FROM admins WHERE email=$1 AND admin_id!=$2"
+        values = [email, user_id]
+        result = await client.query(query, values)
+        if (result.rowCount > 0) {
+            console.log("5")
+            return false
         } else {
-            console.log("2")
-            if (usertype == 'seller') {
-                query = "SELECT seller_id FROM sellers WHERE email=$1 AND seller_id!=$2"
-                values = [email, user_id]
-                result = await client.query(query, values)
-                if (result.rowCount > 0) {
-                    console.log("3")
-                    console.log(result.rows[0])
-                    return false
-                } else {
-                    console.log("4")
-                    if (usertype == 'admin') {
-                        query = "SELECT admin_id FROM admins WHERE email=$1 AND admin_id!=$2"
-                        values = [email, user_id]
-                        result = await client.query(query, values)
-                        if (result.rowCount > 0) {
-                            console.log("5")
-                            return false
-                        } else {
-                            console.log("6")
-                            return true
-                        }
-                    }
-                }
-            }
+            console.log("6")
+            return true
         }
     }
 }
