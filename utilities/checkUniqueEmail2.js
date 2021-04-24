0113896eed8ed469bf2ff66e99eb3997a5b57511
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-25 00:24:45 +0500

    improved checkUniqueEmail2

--- utilities/checkUniqueEmail2.js
+++ utilities/checkUniqueEmail2.js
@@ -7,30 +7,29 @@ const checkUniqueEmail2 = async(email, usertype, user_id) => {
     if (usertype == 'customer') {
         query = "SELECT customer_id FROM customers WHERE email=$1 AND customer_id!=$2"
         values = [email, user_id]
-    }
-    let result = await client.query(query, values)
-    if (result.rowCount > 0) {
-        return false
-    } else {
 
-        if (usertype == 'seller') {
-            query = "SELECT seller_id FROM sellers WHERE email=$1 AND seller_id!=$2"
-            values = [email, user_id]
-        }
-        result = await client.query(query, values)
+        let result = await client.query(query, values)
         if (result.rowCount > 0) {
             return false
         } else {
-
-            if (usertype == 'admin') {
-                query = "SELECT admin_id FROM admins WHERE email=$1 AND admin_id!=$2"
+            if (usertype == 'seller') {
+                query = "SELECT seller_id FROM sellers WHERE email=$1 AND seller_id!=$2"
                 values = [email, user_id]
-            }
-            result = await client.query(query, values)
-            if (result.rowCount > 0) {
-                return false
-            } else {
-                return true
+                result = await client.query(query, values)
+                if (result.rowCount > 0) {
+                    return false
+                } else {
+                    if (usertype == 'admin') {
+                        query = "SELECT admin_id FROM admins WHERE email=$1 AND admin_id!=$2"
+                        values = [email, user_id]
+                        result = await client.query(query, values)
+                        if (result.rowCount > 0) {
+                            return false
+                        } else {
+                            return true
+                        }
+                    }
+                }
             }
         }
     }
