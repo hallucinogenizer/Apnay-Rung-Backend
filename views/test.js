fddc62d3f8503c678e9e9797c40e97b088fd729e
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2021-04-25 18:18:45 +0500

    changed fileupload test

--- views/test.js
+++ views/test.js
@@ -10,7 +10,7 @@ document.querySelector("#form").addEventListener("submit", (e) => {
         },
         body: new FormData(document.querySelector("#form"))
     }).then(response => {
-        alert("Hi")
+        alert(response.status)
         console.log(response)
     }).catch(err => {
         console.log(err.json())
@@ -18,38 +18,38 @@ document.querySelector("#form").addEventListener("submit", (e) => {
 })
 
 
-document.querySelector("#form2").addEventListener("submit", (e) => {
-    e.stopImmediatePropagation()
-    e.preventDefault()
-    fetch(document.querySelector("#form2").action, {
-        method: "POST",
-        withCredentials: true,
-        credentials: "include",
-        headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsIm5hbWUiOiJWYWZhIEJhdG9vbCIsInR5cGVPZlVzZXIiOiJzZWxsZXIiLCJpYXQiOjE2MTkxMjA5MDJ9.GaloR9PH0mSBJBwAIXoqZFRSY96HlEsQCqhnO23TzZE`
-        },
-        body: new FormData(document.querySelector("#form2"))
-    }).then(response => {
-        console.log(response)
-        alert("Hi")
-    }).catch(err => {
-        console.log(err)
-    })
-})
+// document.querySelector("#form2").addEventListener("submit", (e) => {
+//     e.stopImmediatePropagation()
+//     e.preventDefault()
+//     fetch(document.querySelector("#form2").action, {
+//         method: "POST",
+//         withCredentials: true,
+//         credentials: "include",
+//         headers: {
+//             Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsIm5hbWUiOiJWYWZhIEJhdG9vbCIsInR5cGVPZlVzZXIiOiJzZWxsZXIiLCJpYXQiOjE2MTkxMjA5MDJ9.GaloR9PH0mSBJBwAIXoqZFRSY96HlEsQCqhnO23TzZE`
+//         },
+//         body: new FormData(document.querySelector("#form2"))
+//     }).then(response => {
+//         console.log(response)
+//         alert("Hi")
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// })
 
 
-document.querySelector("#form3").addEventListener("submit", (e) => {
-    e.stopImmediatePropagation()
-    e.preventDefault()
-    fetch(document.querySelector("#form2").action, {
-        method: "POST",
-        withCredentials: true,
-        credentials: "include",
-        body: new FormData(document.querySelector("#form3"))
-    }).then(response => {
-        console.log(response)
-        alert(response.status)
-    }).catch(err => {
-        console.log(err)
-    })
-})
+// document.querySelector("#form3").addEventListener("submit", (e) => {
+//     e.stopImmediatePropagation()
+//     e.preventDefault()
+//     fetch(document.querySelector("#form2").action, {
+//         method: "POST",
+//         withCredentials: true,
+//         credentials: "include",
+//         body: new FormData(document.querySelector("#form3"))
+//     }).then(response => {
+//         console.log(response)
+//         alert(response.status)
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// })
